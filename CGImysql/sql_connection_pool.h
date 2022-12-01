baedb9bf3ace738ace273862e8fa6129a8477725
Yap Miracle <2049829115@qq.com>
2022-12-01 19:07:09 +0800

    写到了SQLCONNECTIONPOOL_H

---
+++ CGImysql/sql_connection_pool.h
@@ -0,0 +1,21 @@
+/*
+ * @Author: mirac
+ * @Date: 2022-12-1 16.58
+ * @LastEditTime: 2022-12-1
+ * @LastEditors: mirac
+ */
+
+#ifndef WEBSERVER_SQLCONNECTIONPOOL_H
+#define WEBSERVER_SQLCONNECTIONPOOL_H
+
+#include <cstdio>
+#include <list>
+#include <error.h>
+#include <mysql/mysql.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include "../lock/locker.h"
+#include 
+
+#endif // WEBSERVER_SQLCONNECTIONPOOL_H
---
+++ threadpool/threadpool.h
@@ -0,0 +1,23 @@
+/*
+ * @Author: mirac
+ * @Date: 2022-12-1 16.49
+ * @LastEditTime: 2022-12-1
+ * @LastEditors: mirac
+ */
+
+#ifndef WEBSERVER_THREADPOOL_H
+#define WEBSERVER_THREADPOOL_H
+
+#include <list>
+#include <cstdio>
+#include <exception>
+#include <pthread.h>
+#include "../lock/locker.h"
+
+template <typename T>
+class threadpool{
+public:
+    threadpool(int actor_model, )
+};
+
+#endif // WEBSERVER_THREADPOOL_H
