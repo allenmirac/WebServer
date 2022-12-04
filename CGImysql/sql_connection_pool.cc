f53fa182f7f9bf817d46dc8e1a365e85d81e08fd
Yap Miracle <2049829115@qq.com>
2022-12-04 12:31:47 +0800

    :fire: conn pool

--- CGImysql/sql_connection_pool.cc
+++ CGImysql/sql_connection_pool.cc
@@ -12,5 +12,89 @@ connection_pool *connection_pool::GetInstance(){
 }
 
 void connection_pool::init(std::string url, std::string user, std::string password, std::string dataBaseName, int port, int maxConn, int close_log) {
-    
+    m_url=url;
+    m_user=user;
+    m_password=password;
+    m_databaseName=dataBaseName;
+    m_port=port;
+    m_close_log=close_log;
+
+    for(int i=0; i<maxConn; i++){
+        MYSQL *con = nullptr;
+        con = mysql_init(con);
+        if(nullptr == con){
+            LOG_ERROR("Mysql Error");
+            exit(1);
+        }
+        con = mysql_real_connect(con, url.c_str(), user.c_str(), password.c_str(), dataBaseName.c_str(), port, NULL, 0);
+        if(nullptr == con){
+            LOG_ERROR("Mysql Error");
+            exit(1);
+        }
+        connList.push_back(con);
+        ++m_freeConn;
+    }
+    reserve = sem(m_freeConn);
+    m_maxConn=m_freeConn;
+}
+
+MYSQL *connection_pool::GetConnection(){
+    MYSQL* con=nullptr;
+    if(0 == connList.size()){
+        return nullptr;
+    }
+    reserve.wait();
+
+    lock.lock();
+    con = connList.front();
+    connList.pop_front();
+    --m_freeConn;
+    ++m_curConn;
+    lock.unlock();
+
+    return con;
+}
+
+bool connection_pool::ReleaseConnection(MYSQL* con){
+    if(nullptr == con){
+        return false;
+    }
+
+    lock.lock();
+    connList.push_back(con);// the size of connList can't be change.
+    ++m_freeConn;
+    --m_curConn;
+    lock.unlock();
+
+    reserve.post();
+    return true;
+}
+
+void connection_pool::DestroyPool(){
+    lock.lock();
+    if(connList.size() > 0){
+        for(auto it=connList.begin(); it!=connList.end(); it++){
+            MYSQL *con = *it;
+            mysql_close(con);
+        }
+        m_curConn=0;
+        m_maxConn=0;
+        connList.clear();
+    }
+    lock.unlock();
+}
+
+connection_pool::~connection_pool(){
+    DestroyPool();
+}
+
+connectionRAII::connectionRAII(MYSQL **SQL, connection_pool *pool) {
+    *SQL = pool->GetConnection();
+
+    connRAII = *SQL;
+    poolRAII = pool;
+}
+
+connectionRAII::~connectionRAII(){
+    poolRAII->ReleaseConnection(connRAII);
 }
--- lock/locker.h
+++ lock/locker.h
@@ -40,13 +40,16 @@ private:
     pthread_mutex_t m_mutex;
 };
 
+// wait is same as P, post is the same as V in OS;
 class sem {
 public:
+    // semaphore=0;
     sem(){
         if(sem_init(&m_sem, 0, 0)!=0){
             throw std::exception();
         }
     }
+    // semaphore=num;
     sem(int num) {
         if(sem_init(&m_sem, 0, num)!=0){
             throw std::exception();
@@ -55,11 +58,11 @@ public:
     ~sem(){
         sem_destroy(&m_sem);
     }
-
+    // semaphore--;
     inline bool wait(){
         return sem_wait(&m_sem);
     }
-
+    // semaphore++;
     inline bool post(){
         return sem_post(&m_sem) == 0;
     }
