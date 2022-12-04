6aba61687813b20f7ebaf3d095d5e8d60185e78a
Yap Miracle <2049829115@qq.com>
2022-12-04 10:58:09 +0800

    :fire: test mysql connection

---
+++ test/connMysql.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <mysql/mysql.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	MYSQL conn;
+	mysql_init(&conn);
+
+	if (!mysql_real_connect(&conn, "localhost", "test", "", "user", 3306, NULL, 0))
+	{
+		cout << "mysql connect failed" << endl;
+		exit(-1);
+	}
+	cout << "mysql conenct success" << endl;
+	mysql_close(&conn);
+	return 0;
+}
+/* 
+g++ -o connMysql connMysql.cc -I /usr/include/mysql -lmysqlclient
+ */
